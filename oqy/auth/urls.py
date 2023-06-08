c87c8b4af04003ff5ab39c77aaa083f4d12edc88
arsenozhetov <a.ozhetov@arbuz.kz>
2023-06-08 14:33:39 +0600

    refactor: make code

--- oqy/auth/urls.py
+++ oqy/auth/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from oqy.auth.views import RegisterUserView, LoginView
 
 urlpatterns = [
-    path('register/', RegisterUserView.as_view(), name='auth-register'),
-    path('login/', LoginView.as_view(), name='auth-login'),
+    path("register/", RegisterUserView.as_view(), name="auth-register"),
+    path("login/", LoginView.as_view(), name="auth-login"),
 ]
--- oqy/auth/views.py
+++ oqy/auth/views.py
@@ -1,30 +1,30 @@
 from rest_framework.views import APIView
-from oqy.core.application.auth.services import AuthenticationService
+from oqy.auth.services import AuthenticationService
 
 
 class RegisterUserView(APIView):
     def post(self, request) -> JsonResponse:
-        username = request.data.get('username')
-        email = request.data.get('email')
-        password = request.data.get('password')
+        username = request.data.get("username")
+        email = request.data.get("email")
+        password = request.data.get("password")
 
         user_repository = DjangoUserRepository()
         authentication_service = AuthenticationService(user_repository)
         user = authentication_service.register_user(username, email, password)
 
-        return JsonResponse({'message': 'User registered successfully'})
+        return JsonResponse({"message": "User registered successfully"})
 
 
 class LoginView(APIView):
     def post(self, request) -> JsonResponse:
-        username = request.data.get('username')
-        password = request.data.get('password')
+        username = request.data.get("username")
+        password = request.data.get("password")
 
         user_repository = DjangoUserRepository()
         authentication_service = AuthenticationService(user_repository)
         user = authentication_service.authorize_user(username, password)
 
         if user:
-            return JsonResponse({'message': 'Login successful'})
+            return JsonResponse({"message": "Login successful"})
 
-        return JsonResponse({'message': 'Invalid credentials'}, status=401)
+        return JsonResponse({"message": "Invalid credentials"}, status=401)
